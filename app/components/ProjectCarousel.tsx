cd4a1818a0fe2b01e9ce8d359bff578936f9cfc0
jiin9999 <cji3604@gmail.com>
2023-07-10 13:09:28 +0900

    fix: 캐러셀 비활성화

--- app/components/ProjectCarousel.tsx
+++ app/components/ProjectCarousel.tsx
@@ -9,21 +9,20 @@ interface CarouselProps {
 }
 
 export default function ProjectCarousel({ imageNames }: CarouselProps) {
-  useEffect(() => {
-    register()
-  }, [])
-
-  return (
-    <swiper-container slides-per-view="1">
-      {imageNames.map((imageName, i) => (
-        <swiper-slide key={i}>
-          <Image
-            src={`/projectImage/${imageName}.avif`}
-            alt={`프로젝트 ${i + 1}번째 소개 이미지`}
-            fill={true}
-          />
-        </swiper-slide>
-      ))}
-    </swiper-container>
-  )
+  // useEffect(() => {
+  //   register()
+  // }, [])
+  // return (
+  //   <swiper-container slides-per-view="1">
+  //     {imageNames.map((imageName, i) => (
+  //       <swiper-slide key={i}>
+  //         <Image
+  //           src={`/projectImage/${imageName}.avif`}
+  //           alt={`프로젝트 ${i + 1}번째 소개 이미지`}
+  //           fill={true}
+  //         />
+  //       </swiper-slide>
+  //     ))}
+  //   </swiper-container>
+  // )
 }
